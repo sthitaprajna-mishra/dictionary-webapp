cc034c27bd283e79e85437145ddce86f505be18d
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-10-05 22:16:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      {/* Wrapper */}
+      <div className="border-2 border-orange-600 min-h-screen grid grid-cols-12">
+        {/* Container */}
+        <div className="border-2 border-green-600 col-start-3 col-span-8">
+          <h1>Hello World</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
