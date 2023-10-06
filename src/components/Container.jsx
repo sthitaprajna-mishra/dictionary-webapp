2e03d53a75b40c288e5ee9c5685a352c6daccfdc
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-10-06 22:57:42 +0530

    added axios

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,15 +1,46 @@
+// react
+import { useEffect, useState } from "react";
+
 // components
 import Header from "./Header";
 import Searchbar from "./Searchbar";
 import WordHeading from "./WordHeading";
 
+// axios
+import axios from "axios";
+
+const baseURL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
 const Container = () => {
+  const [searchedWord, setSearchedWord] = useState();
+  const [result, setResult] = useState();
+
+  useEffect(() => {
+    const apiURL = `${baseURL}${searchedWord}`;
+
+    console.log(searchedWord);
+
+    axios
+      .get(apiURL)
+      .then((response) => {
+        console.log(response);
+        const apiResponse = response.data[0];
+        console.log(apiResponse);
+        setResult(apiResponse);
+      })
+      .catch((err) => {
+        console.log("inside error block");
+        console.log(err);
+        console.log(err.response.data);
+      });
+  }, [searchedWord]);
+
   return (
     <div className="border-1 border-green-600 col-span-12 mx-4 md:col-start-3 md:col-span-8 xl:col-start-4 xl:col-span-6">
       {/* Header */}
       <Header />
       {/* Searchbar */}
-      <Searchbar />
+      <Searchbar setSearchedWord={setSearchedWord} />
       {/* Word Heading */}
       <WordHeading />
     </div>
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,19 +1,44 @@
 // assets
 import iconsearch from "../assets/icon-search.svg";
 
-const Searchbar = () => {
+// react
+import { useState } from "react";
+
+const Searchbar = ({ setSearchedWord }) => {
+  const [search, setSearch] = useState("");
+
+  const handleClick = (e) => {
+    setSearchedWord(search);
+  };
+
+  const handleKeyChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleKeyDownPress = (e) => {
+    if (e.key === "Enter" && search.trim() !== "") {
+      setSearchedWord(search);
+    }
+  };
+
   return (
     <div className="bg-inputColor rounded-xl flex items-center mt-10 py-4 outline-none focus-within:outline focus-within:outline-1 focus-within:outline-offset-0 focus-within:outline-accentColor">
       {/* Input */}
       <div className="w-11/12 pl-4 md:text-xl">
         <input
+          value={search}
+          onChange={(e) => handleKeyChange(e)}
+          onKeyDown={handleKeyDownPress}
           className="w-full bg-inherit text-fontColor font-semibold placeholder:text-placeholderColor placeholder:font-normal outline-none"
           type="text"
           placeholder="Search for a word"
         />
       </div>
       {/* Search button */}
-      <div className="w-1/12 flex items-center justify-center hover:cursor-pointer transition-all mr-2 md:mr-0">
+      <div
+        className="w-1/12 flex items-center justify-center hover:cursor-pointer transition-all mr-2 md:mr-0"
+        onClick={(e) => handleClick(e)}
+      >
         <img src={iconsearch} alt="search icon" />
       </div>
     </div>
